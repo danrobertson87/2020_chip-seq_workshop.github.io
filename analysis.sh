dc631d6f06a804579099794f951cdcd7fc327210
danrobertson87 <daniel.robertson@ed.ac.uk>
2020-11-09 18:05:38 +0000

    Init

---
+++ analysis.sh
@@ -0,0 +1,26 @@
+##DEEPTOOLS
+
+##Correlation
+multiBamSummary bins -b bwa_out/*/*uniq.bam -bs 10000 -o cov.matrix -e 200 --smartLabels
+plotCorrelation -in cov.matrix -p heatmap -c spearman -o spearman_cor.png
+##Fingerprint
+#Leave commented for tutorial as takes a long time to run
+#plotFingerprint -b bwa_out/*/*uniq.bam -e 200 --smartLabels -o fingerprint.png 
+##Normalised ChIP vs Input scores
+bamCompare -b1 bwa_out/Reb1_R1/Reb1_R1.uniq.bam -b2 bwa_out/Input_R1/Input_R1.uniq.bam -o Reb1_R1.norm.bw -e 200 --operation log2 --normalizeUsing BPM --scaleFactorsMethod None
+bamCompare -b1 bwa_out/Reb1_R2/Reb1_R2.uniq.bam -b2 bwa_out/Input_R2/Input_R2.uniq.bam -o Reb1_R2.norm.bw -e 200 --operation log2 --normalizeUsing BPM --scaleFactorsMethod None
+ln -s $PWD/*norm.bw visualisation
+##Get gene annotations
+ln -s ~genomes/s.cerevisiae/sacCer3/annotation/UCSC_sgdGene.bed .
+##TSS plot
+computeMatrix reference-point -R UCSC_sgdGene.bed -S Reb1_R1.norm.bw Reb1_R2.norm.bw -o TSS.matrix --referencePoint TSS --upstream 2000 --downstream 2000 -bs 100 --smartLabels
+plotHeatmap -m TSS.matrix -o TSS.heatmap.png
+
+##PEAK CALLING
+
+##macs
+macs2 callpeak -t bwa_out/Reb1_R1/Reb1_R1.uniq.bam bwa_out/Reb1_R2/Reb1_R2.uniq.bam -c bwa_out/Input_R1/Input_R1.uniq.bam bwa_out/Input_R2/Input_R2.uniq.bam -g 12000000 --nomodel -n Reb1
+##Get sequences under peaks
+fastaFromBed -fi ~genomes/s.cerevisiae/sacCer3/sacCer3.fa -bed Reb1_peaks.narrowPeak -fo Reb1_peaks.fasta
+##MEME
+meme-chip Reb1_peaks.fasta
---
+++ pipeline.sh
@@ -0,0 +1,55 @@
+##PREPARATION
+
+##Create a web directory to view output                         
+ln -s $PWD ~/public_html/
+##Create a folder for fastq files
+mkdir fastq
+##Link the raw data to the fastq folder
+ln -s /homes/library/training/ChIP-seq_workshop/data/*fq.gz fastq/
+
+##QC
+
+##FastQ Screen
+cat samples.txt | parallel -j 4 fastq_screen fastq/{}.fq.gz --outdir fastq
+##FastQC
+cat samples.txt | parallel -j 4 fastqc fastq/{}.fq.gz
+##Multi QC
+multiqc -o fastq fastq
+
+##PREPROCESSING
+
+##Cutadapt
+cat samples.txt | parallel -j 4 cutadapt -a AGATCGGAAGAG -q 20 --minimum-length 36 -o fastq/{}.trim.fq.gz fastq/{}.fq.gz > fastq/{}.trim.cutadapt_report.txt
+
+##FastQC
+cat samples.txt | parallel -j 4 fastqc fastq/{}.trim.fq.gz
+multiqc -f -o fastq fastq
+
+##ALIGNMENT
+
+##BWA
+mkdir bwa_out
+cat samples.txt | parallel -j 4 mkdir bwa_out/{}
+cat samples.txt | parallel -j 4 "bwa mem -t 5 -R '@RG\tID:{}\tPL:ILLUMINA' -M ~genomes/s.cerevisiae/sacCer3/bwa_indexes/sacCer3 fastq/{}.trim.fq.gz > bwa_out/{}/{}.sam"
+##Sort and index
+cat samples.txt | parallel -j 4 "samtools sort bwa_out/{}/{}.sam -o bwa_out/{}/{}.bam -T bwa_out/{}/{} -O BAM; samtools index bwa_out/{}/{}.bam"
+##Filter for uniquely mapped reads
+cat samples.txt | parallel -j 4 "samtools view -b -q 20 bwa_out/{}/{}.bam -o bwa_out/{}/{}.uniq.bam; samtools index bwa_out/{}/{}.uniq.bam"
+##FastQC on BAM files
+cat samples.txt | parallel -j 4 fastqc bwa_out/{}/{}.uniq.bam
+multiqc -o bwa_out bwa_out
+
+##VISUALISATION
+
+##Convert BAM to normalised bigWig
+cat samples.txt | parallel -j 4 bamCoverage -bs 1 --normalizeUsing BPM -e 200 -b bwa_out/{}/{}.uniq.bam --outFileName bwa_out/{}/{}.uniq.bw
+
+##Make visualisation directory
+mkdir visualisation
+ln -s $PWD/bwa_out/*/*bw visualisation
+ln -s $PWD/bwa_out/*/*uniq.bam* visualisation
+
+##Tidy up
+rm fastq/*trim*fq.gz #Remove trimmed fastq temp files
+rm bwa_out/*/*.sam #Remove sam files
+
