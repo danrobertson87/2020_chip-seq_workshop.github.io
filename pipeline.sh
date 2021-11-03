9096f9d57d71f18d14de21acddd494da6486b212
danrobertson87 <daniel.robertson@ed.ac.uk>
2021-11-03 17:51:22 +0000

    2021 updates

--- pipeline.sh
+++ pipeline.sh
@@ -1,16 +1,16 @@
 ##PREPARATION
 
-##Create a web directory to view output                         
+##Create a web directory to view output
 ln -s $PWD ~/public_html/
 ##Create a folder for fastq files
 mkdir fastq
 ##Link the raw data to the fastq folder
-ln -s /homes/library/training/ChIP-seq_workshop/data/*fq.gz fastq/
+cp /homes/library/training/ChIP-seq_workshop/data/*fq.gz fastq/.
 
 ##QC
 
 ##FastQ Screen
-cat samples.txt | parallel -j 4 fastq_screen fastq/{}.fq.gz --outdir fastq
+cat samples.txt | parallel -j 4 "fastq_screen  --conf /homes/genomes/tool_configs/fastq_screen/fastq_screen.conf fastq/{}.fq.gz --outdir fastq"
 ##FastQC
 cat samples.txt | parallel -j 4 fastqc fastq/{}.fq.gz
 ##Multi QC
@@ -19,7 +19,8 @@ multiqc -o fastq fastq
 ##PREPROCESSING
 
 ##Cutadapt
-cat samples.txt | parallel -j 4 cutadapt -a AGATCGGAAGAG -q 20 --minimum-length 36 -o fastq/{}.trim.fq.gz fastq/{}.fq.gz > fastq/{}.trim.cutadapt_report.txt
+cat samples.txt | parallel -j 4 "cutadapt -a AGATCGGAAGAG -q 20 --minimum-length 36 -o fastq/{}.trim.fq.gz fastq/{}.fq.gz > fastq/{}.trim.cutadapt_report.txt"
+
 
 ##FastQC
 cat samples.txt | parallel -j 4 fastqc fastq/{}.trim.fq.gz
@@ -30,7 +31,7 @@ multiqc -f -o fastq fastq
 ##BWA
 mkdir bwa_out
 cat samples.txt | parallel -j 4 mkdir bwa_out/{}
-cat samples.txt | parallel -j 4 "bwa mem -t 5 -a -R '@RG\tID:{}\tPL:ILLUMINA' -M ~genomes/s.cerevisiae/sacCer3/bwa_indexes/sacCer3 fastq/{}.trim.fq.gz > bwa_out/{}/{}.sam"
+cat samples.txt | parallel -j 4 "bwa mem -t 5 -a -R '@RG\tID:{}\tPL:ILLUMINA' -M /homes/genomes/s.cerevisiae/sacCer3/bwa_indexes/sacCer3 fastq/{}.trim.fq.gz > bwa_out/{}/{}.sam"
 ##Sort and index
 cat samples.txt | parallel -j 4 "samtools sort bwa_out/{}/{}.sam -o bwa_out/{}/{}.bam -T bwa_out/{}/{} -O BAM; samtools index bwa_out/{}/{}.bam"
 ##Filter for uniquely mapped reads
